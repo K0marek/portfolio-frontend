cad7274bca675566a5f3c03d61501ec7fac3f835
Mateusz Kleszcz <kleeeszcz@gmail.com>
2020-06-29 17:45:43 +0200

    remove trash files

--- static/Main.js
+++
@@ -1,8 +0,0 @@
-var net;
-var ui;
-$(document).ready(function () {
-    net = new Net() // utworzenie obiektu klasy Net
-    ui = new Ui() // utworzenie obiektu klasy Ui
-    music = new Music()
-    visual = new Visual()
-})
