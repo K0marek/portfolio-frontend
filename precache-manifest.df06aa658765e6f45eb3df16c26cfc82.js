da98019334328ac45f4d5787ccbad53be382c768
Mateusz Kleszcz <kleeeszcz@gmail.com>
2020-07-03 12:17:53 +0200

    Updates

---
+++ precache-manifest.df06aa658765e6f45eb3df16c26cfc82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df879fbffb577a8e12b0ae2f911a09b1",
+    "url": "/portfolio-frontend/index.html"
+  },
+  {
+    "revision": "73000b9a64ca1bc3864c",
+    "url": "/portfolio-frontend/static/css/main.ae763f24.chunk.css"
+  },
+  {
+    "revision": "0f2cddabc378e943fa8c",
+    "url": "/portfolio-frontend/static/js/2.197f076b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-frontend/static/js/2.197f076b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73000b9a64ca1bc3864c",
+    "url": "/portfolio-frontend/static/js/main.35005d0c.chunk.js"
+  },
+  {
+    "revision": "f2c6a17b7e383a460c74",
+    "url": "/portfolio-frontend/static/js/runtime-main.7c423e37.js"
+  }
+]);
